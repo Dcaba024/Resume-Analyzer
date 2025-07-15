446e023d5222171ba6eac906dce458724d25b6ad
Dylan Caballero <dylan@Dylans-MacBook-Pro.local>
2025-07-15 06:42:00 -0400

    stable production build

--- lib/pdfParser.ts
+++ lib/pdfParser.ts
@@ -4,16 +4,19 @@ import { getDocument, GlobalWorkerOptions, version } from 'pdfjs-dist/webpack'
 GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${version}/pdf.worker.min.js`
 
 export async function extractTextFromPDF(file: File): Promise<string> {
-  const arrayBuffer = await file.arrayBuffer()
-  const pdf = await getDocument({ data: arrayBuffer }).promise
-  let finalText = ''
-
-  for (let pageNum = 1; pageNum <= pdf.numPages; pageNum++) {
-    const page = await pdf.getPage(pageNum)
-    const content = await page.getTextContent()
-    const strings = content.items.map((item: { str: string }) => item.str)
-    finalText += strings.join(' ') + '\n'
+    const arrayBuffer = await file.arrayBuffer()
+    const pdf = await getDocument({ data: arrayBuffer }).promise
+    let finalText = ''
+  
+    for (let pageNum = 1; pageNum <= pdf.numPages; pageNum++) {
+      const page = await pdf.getPage(pageNum)
+      const content = await page.getTextContent()
+      const strings = content.items
+        .map((item) => ('str' in item ? item.str : ''))
+        .filter((text) => text.length > 0)
+      finalText += strings.join(' ') + '\n'
+    }
+  
+    return finalText
   }
-
-  return finalText
-}
+  
